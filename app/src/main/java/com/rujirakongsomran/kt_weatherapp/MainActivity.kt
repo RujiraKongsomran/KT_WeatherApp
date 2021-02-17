c7a342d0b78dc3f085d47f7b5da1e6f3e7d64d39
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-02-17 22:27:41 +0700

    Finish

--- app/src/main/java/com/rujirakongsomran/kt_weatherapp/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_weatherapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.rujirakongsomran.kt_weatherapp
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.rujirakongsomran.kt_weatherapp.databinding.ActivityMainBinding
+import org.json.JSONObject
+import java.lang.Exception
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    val CITY: String = "Bangkok"
+    val APIKEY: String = "2836efc2a9c6e43e9f89fa9510aa41dd"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +23,76 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        weatherTask().execute()
+    }
+
+    inner class weatherTask() : AsyncTask<String, Void, String>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            binding.pbLoader.visibility = View.VISIBLE
+            binding.mainContainer.visibility = View.GONE
+            binding.tvErrorText.visibility = View.GONE
+        }
+
+        override fun doInBackground(vararg params: String?): String? {
+            var response: String?
+            try {
+                response =
+                    URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$APIKEY")
+                        .readText(Charsets.UTF_8)
+            } catch (e: Exception) {
+                response = null
+            }
+            return response
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            try {
+                val jsonObj = JSONObject(result)
+                val main = jsonObj.getJSONObject("main")
+                val sys = jsonObj.getJSONObject("sys")
+                val wind = jsonObj.getJSONObject("wind")
+                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+                val updateAt: Long = jsonObj.getLong("dt")
+                val updateAtText =
+                    "Update at: " + SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(
+                        Date(updateAt * 1000)
+                    )
+                val separateTemp = main.getString("temp").split(".")
+                val temp = separateTemp[0] + "°C"
+                val tempMin = "Min Temp: " + main.getString("temp_min") + "°C"
+                val tempMax = "Max Temp: " + main.getString("temp_max") + "°C"
+                val pressure = main.getString("pressure")
+                val humidity = main.getString("humidity")
+                val sunrise: Long = sys.getLong("sunrise")
+                val sunset: Long = sys.getLong("sunset")
+                val windSpeed = wind.getString("speed")
+                val weatherDescription = weather.getString("description")
+                val address = jsonObj.getString("name") + ", " + sys.getString("country")
+
+                binding.tvAddress.text = address
+                binding.tvUpdateAt.text = updateAtText
+                binding.tvStatus.text = weatherDescription.capitalize()
+                binding.tvTemp.text = temp
+                binding.tvTempMin.text = tempMin
+                binding.tvTempMax.text = tempMax
+                binding.tvSunrise.text =
+                    SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise * 1000))
+                binding.tvSunset.text =
+                    SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset * 1000))
+                binding.tvWind.text = windSpeed
+                binding.tvPressure.text = pressure
+                binding.tvHumidity.text = humidity
+
+                binding.pbLoader.visibility = View.GONE
+                binding.mainContainer.visibility = View.VISIBLE
+
+            } catch (e: Exception) {
+                binding.pbLoader.visibility = View.GONE
+                binding.tvErrorText.visibility = View.VISIBLE
+            }
+        }
     }
 }
